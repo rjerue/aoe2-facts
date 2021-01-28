43429a564d3dbc7769188db96d3e6db38ca1caa5
rjerue <ryan.jerue@reifyhealth.com>
2021-01-28 11:54:06 -0500

    nope

--- resources/public/js/compiled/cljs-runtime/aoe2.config.js
+++
@@ -1,4 +0,0 @@
-goog.provide('aoe2.config');
-aoe2.config.debug_QMARK_ = goog.DEBUG;
-
-//# sourceMappingURL=aoe2.config.js.map
--- resources/public/js/compiled/cljs-runtime/aoe2.db.js
+++
@@ -1,4 +0,0 @@
-goog.provide('aoe2.db');
-aoe2.db.default_db = new cljs.core.PersistentArrayMap(null, 5, [new cljs.core.Keyword(null,"name","name",1843675177),"re-frame",new cljs.core.Keyword("aoe2.db","active-panel","aoe2.db/active-panel",1662803397),new cljs.core.Keyword(null,"home-panel","home-panel",1226198754),new cljs.core.Keyword(null,"civilizations","civilizations",822016090),cljs.core.PersistentArrayMap.EMPTY,new cljs.core.Keyword(null,"units","units",-533089095),cljs.core.PersistentArrayMap.EMPTY,new cljs.core.Keyword(null,"civilizations-loading","civilizations-loading",-1811779656),false], null);
-
-//# sourceMappingURL=aoe2.db.js.map
